df8faa439aba876f0bcb12ac8b94668cbe54280a
Noah <noahfrost@outlook.com>
2025-03-18 20:10:00 +0000

    Update

--- Project-1-IaC-Security/main.tf
+++ Project-1-IaC-Security/main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "eu-west-2"
 }
 
-# Create an S3 bucket
+# Create an S3 bucket for the infrastructure
 resource "aws_s3_bucket" "project_1_iac_security" {
   bucket        = "project-1-iac-security"
   force_destroy = true
@@ -32,4 +32,13 @@ resource "aws_s3_bucket_logging" "s3_logging" {
   bucket        = aws_s3_bucket.project_1_iac_security.id
   target_bucket = aws_s3_bucket.project_1_iac_security.id
   target_prefix = "log/"
+}
+
+# Terraform Backend Configuration (Store the Terraform state in S3)
+terraform {
+  backend "s3" {
+    bucket = "project-1-iac-security"  # Your bucket name
+    key    = "terraform/state/project1/terraform.tfstate"  # The path where the state file will be stored
+    region = "eu-west-2"  # The AWS region where your bucket is located
+  }
 }
