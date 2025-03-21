62568b79419f38ed4007dd33b7cc0389ec55cf4b
Noah <noahfrost@outlook.com>
2025-03-21 17:28:46 +0000

    Update

--- Project-1-IaC-Security/main.tf
+++ Project-1-IaC-Security/main.tf
@@ -105,6 +105,11 @@ resource "aws_s3_bucket_lifecycle_configuration" "state_bucket_lifecycle" {
     id     = "AbortIncompleteMultipart"
     status = "Enabled"
 
+    # Filter block required by the provider; empty prefix applies the rule to all objects.
+    filter {
+      prefix = ""
+    }
+
     abort_incomplete_multipart_upload {
       days_after_initiation = 7
     }
@@ -118,10 +123,9 @@ resource "aws_iam_group" "admins" {
   name = "admins"
 }
 
-resource "aws_iam_group_policy_attachment" "admin_full_access" {
+resource "aws_iam_group_policy_attachment" "admin_full_access" { # checkov:skip=CKV_AWS_274: AdministratorAccess is intentionally used for demo purposes.
   group      = aws_iam_group.admins.name
   policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
-  # checkov:skip=CKV_AWS_274: AdministratorAccess is intentionally used for demo purposes.
 }
 
 resource "aws_iam_group" "developers" {
