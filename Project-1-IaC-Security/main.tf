9a3fe004c8366ef5f2ae2d0f818275d6b8ff1811
Noah <noahfrost@outlook.com>
2025-03-21 17:11:33 +0000

    Update

--- Project-1-IaC-Security/main.tf
+++ Project-1-IaC-Security/main.tf
@@ -29,17 +29,16 @@ resource "aws_kms_key" "state_bucket_kms" {
   description         = "KMS key for Terraform state bucket encryption"
   enable_key_rotation = true
 
-  # Minimal policy granting full access to your AWS account root only
   policy = jsonencode({
-    Version   = "2012-10-17"
+    Version   = "2012-10-17",
     Statement = [
       {
-        Sid       = "Allow administration of the key"
-        Effect    = "Allow"
+        Sid       = "Allow administration of the key",
+        Effect    = "Allow",
         Principal = {
           AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
-        }
-        Action   = "kms:*"
+        },
+        Action   = "kms:*",
         Resource = "*"
       }
     ]
@@ -53,13 +52,9 @@ resource "aws_s3_bucket" "state_bucket" {
   bucket        = "my-terraform-state-bucket-devsecops-project"
   force_destroy = true
 
-  # We don't need cross-region replication for a state file bucket.
+  # Skip non-critical checks for a state file bucket:
   # checkov:skip=CKV_AWS_144: Cross-region replication not required for state bucket
-
-  # We don't need event notifications for a state file bucket.
   # checkov:skip=CKV2_AWS_62: Event notifications not required for state bucket
-
-  # We don't need access logging for a state file bucket.
   # checkov:skip=CKV_AWS_18: Access logging not required for state bucket
 }
 
@@ -114,4 +109,34 @@ resource "aws_s3_bucket_lifecycle_configuration" "state_bucket_lifecycle" {
       days_after_initiation = 7
     }
   }
+}
+
+###############################################################################
+# IAM Groups and Policy Attachments
+###############################################################################
+resource "aws_iam_group" "admins" {
+  name = "admins"
+}
+
+resource "aws_iam_group_policy_attachment" "admin_full_access" {
+  group      = aws_iam_group.admins.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
+
+resource "aws_iam_group" "developers" {
+  name = "developers"
+}
+
+resource "aws_iam_group_policy_attachment" "developer_full_access" {
+  group      = aws_iam_group.developers.name
+  policy_arn = "arn:aws:iam::aws:policy/PowerUserAccess"
+}
+
+resource "aws_iam_group" "devsecops_engineers" {
+  name = "devsecops-engineers"
+}
+
+resource "aws_iam_group_policy_attachment" "devsecops_read_only" {
+  group      = aws_iam_group.devsecops_engineers.name
+  policy_arn = "arn:aws:iam::aws:policy/SecurityAudit"
 }
